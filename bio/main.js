9ca2890a30c76b238ee2371a320009c28a7d97aa
Hasnizihar <136541296+hasnizihar@users.noreply.github.com>
2024-06-09 18:08:54 +0530

    Update main.js
    block to flex #mybtn

--- bio/main.js
+++ bio/main.js
@@ -114,7 +114,7 @@ window.onscroll = function() {scrollFunction(),sliding()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    mybutton.style.display = "flex";
   } else {
     mybutton.style.display = "none";
   }
