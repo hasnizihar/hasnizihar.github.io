ae327a0f2161f43bf7c663b8b45e64aa54e79d45
Hasnizihar <136541296+hasnizihar@users.noreply.github.com>
2025-08-07 12:22:38 +0530

    Delete test/portfolio directory

--- test/portfolio/includes/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <h2>My Portfolio</h2>
-    <nav>
-        <a href="index.php">Home</a> |
-        <a href="about.php">About</a> |
-        <a href="contact.php">Contact</a>
-    </nav>
-</header>
