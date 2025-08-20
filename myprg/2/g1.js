d3cfe2626acb0a58b7288dc2eb82c91b28938b29
hasnizihar <hasnizihar751@gmail.com>
2025-08-20 07:55:21 +0530

    renamed:    bio/cv.html -> cv.html 	renamed:    bio/index.html -> index.html 	renamed:    myprg/clc/1.txt -> myprg/1/1.txt 	renamed:    myprg/clc/index.html -> myprg/1/index.html 	renamed:    myprg/clc/script.js -> myprg/1/script.js 	renamed:    myprg/clc/styles.css -> myprg/1/styles.css 	renamed:    myprg/GMN/g1.css -> myprg/2/g1.css 	renamed:    myprg/GMN/g1.js -> myprg/2/g1.js 	renamed:    myprg/GMN/img/img1.jpg -> myprg/2/img/img1.jpg 	renamed:    myprg/GMN/index.html -> myprg/2/index.html 	renamed:    myprg/QRCG/hzgpdfavicin.png -> myprg/3/hzgpdfavicin.png 	renamed:    myprg/QRCG/index.html -> myprg/3/index.html 	renamed:    myprg/QRCG/main.css -> myprg/3/main.css 	renamed:    myprg/QRCG/main.js -> myprg/3/main.js 	renamed:    myprg/RPS/img/icon/pb.png -> myprg/4/img/icon/pb.png 	renamed:    myprg/RPS/img/icon/pw.png -> myprg/4/img/icon/pw.png 	renamed:    myprg/RPS/img/icon/rb.png -> myprg/4/img/icon/rb.png 	renamed:    myprg/RPS/img/icon/rw.png -> myprg/4/img/icon/rw.png 	renamed:    myprg/RPS/img/icon/sb.png -> myprg/4/img/icon/sb.png 	renamed:    myprg/RPS/img/icon/sw.png -> myprg/4/img/icon/sw.png 	renamed:    myprg/RPS/img/img1.jpg -> myprg/4/img/img1.jpg 	renamed:    myprg/RPS/index.html -> myprg/4/index.html 	renamed:    myprg/RPS/main.js -> myprg/4/main.js 	renamed:    myprg/RPS/style.css -> myprg/4/style.css 	renamed:    Past portfolios/1/1.txt -> myprg/5/1.txt 	renamed:    myprg/SS/exit-full-screen.png -> myprg/5/exit-full-screen.png 	renamed:    myprg/SS/full-screen.png -> myprg/5/full-screen.png 	renamed:    myprg/SS/hz w.jpg -> myprg/5/hz w.jpg 	renamed:    myprg/SS/logo.html -> myprg/5/logo.html 	renamed:    myprg/SS/script.js -> myprg/5/script.js 	renamed:    myprg/SS/style.css -> myprg/5/style.css 	renamed:    Past portfolios/2/1.txt -> myprg/6/1.txt 	renamed:    myprg/TDL/accept.png -> myprg/6/accept.png 	renamed:    myprg/TDL/delete.png -> myprg/6/delete.png 	renamed:    myprg/TDL/index.html -> myprg/6/index.html 	renamed:    myprg/TDL/more.png -> myprg/6/more.png 	renamed:    myprg/TDL/pencil.png -> myprg/6/pencil.png 	renamed:    myprg/TDL/todo.css -> myprg/6/todo.css 	renamed:    myprg/TDL/todo.js -> myprg/6/todo.js 	renamed:    Past portfolios/2/bio1/1.txt -> pp/1/1.txt 	renamed:    Past portfolios/1/behance.png -> pp/1/behance.png 	renamed:    Past portfolios/1/facebook.png -> pp/1/facebook.png 	renamed:    Past portfolios/1/github.png -> pp/1/github.png 	renamed:    Past portfolios/1/index.html -> pp/1/index.html 	renamed:    Past portfolios/1/instagram.png -> pp/1/instagram.png 	renamed:    Past portfolios/1/linkedin.png -> pp/1/linkedin.png 	renamed:    Past portfolios/1/profile.jpg -> pp/1/profile.jpg 	renamed:    Past portfolios/1/styles.css -> pp/1/styles.css 	renamed:    myprg/SS/1.txt -> pp/2/1.txt 	renamed:    myprg/TDL/1.txt -> pp/2/bio1/1.txt 	renamed:    Past portfolios/2/bio1/behance.png -> pp/2/bio1/behance.png 	renamed:    Past portfolios/2/bio1/facebook.png -> pp/2/bio1/facebook.png 	renamed:    Past portfolios/2/bio1/github.png -> pp/2/bio1/github.png 	renamed:    Past portfolios/2/bio1/instagram.png -> pp/2/bio1/instagram.png 	renamed:    Past portfolios/2/bio1/linkedin.png -> pp/2/bio1/linkedin.png 	renamed:    Past portfolios/2/bio1/profile.jpg -> pp/2/bio1/profile.jpg 	renamed:    Past portfolios/2/contact.html -> pp/2/contact.html 	renamed:    Past portfolios/2/img1.jpg -> pp/2/img1.jpg 	renamed:    Past portfolios/2/index.html -> pp/2/index.html 	renamed:    Past portfolios/2/script.js -> pp/2/script.js 	renamed:    Past portfolios/2/style.css -> pp/2/style.css 	renamed:    Past portfolios/2/style1.css -> pp/2/style1.css 	new file:   pp/3/index.html 	new file:   pp/3/main.css 	new file:   pp/3/main.js 	new file:   pp/3/prg.css

---
+++ myprg/2/g1.js
@@ -0,0 +1,49 @@
+var uguessnum = document.getElementById('guessnum')
+var cguessnum = document.getElementById('cguessnum')
+var result = document.getElementById('result')
+var wins = document.getElementById('scorew')
+var losses = document.getElementById('scorel')
+var win = 0
+var loss = 0
+
+function check()
+{   if (win == 5){
+    alert("You win! The game is over.")
+    window.location.reload();
+}   else if (loss ==10){
+    alert("You lose! The game is over.")
+    window.location.reload();
+}   else{
+        var randomnum = Math.floor(Math.random()*10)+1
+        var enternum = uguessnum.value
+        if (enternum!=''){
+            if (enternum == randomnum)
+                {
+                    console.log('right')
+                    console.log(randomnum)
+                    console.log(enternum)
+                    result.textContent='You Are Right'
+                    win = win + 1
+                    wins.textContent='Win : '+win
+                    cguessnum.textContent=randomnum
+
+                }
+                else{
+                    console.log('wrong')
+                    console.log(randomnum)
+                    console.log(enternum)
+                    result.textContent='You Are Wrong'
+                    loss = loss + 1
+                    losses.textContent='Loss : '+loss
+                    cguessnum.textContent=randomnum
+
+                }
+        }else{
+            alert('Enter a Number Between 1 to 10')
+        }
+}
+}
+$(window).on("load",function()
+{
+$(".loader-wrapper").fadeOut("slow");
+});
